17f2fab7775cf54c5bf1d42a4bd93f4b2c401e2d
Joseph Idowu <joeyidowu1996@gmail.com>
2025-08-28 01:25:43 +0200

    registration  Page added

---
+++ src/Pages/RegistrationPage.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import AuthForm from '../components/AuthForm/AuthForm';
+
+const RegistrationPage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleRegistration = async (data: any) => {
+    setIsLoading(true);
+    
+
+    console.log('Registration attempt with:', data);
+    
+
+    setTimeout(() => {
+      console.log('Registration successful');
+      setIsLoading(false);
+
+    }, 1000);
+  };
+
+  const handleSwitchToLogin = () => {
+
+    console.log('Switch to login page');
+  };
+
+  return (
+    <AuthForm
+      type="register"
+      onSubmit={handleRegistration}
+      onSwitchForm={handleSwitchToLogin}
+    />
+  );
+};
+
+export default RegistrationPage;
