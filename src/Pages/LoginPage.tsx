3f9b59671a1e11a16be478e337fe064a59c354cb
Joseph Idowu <joeyidowu1996@gmail.com>
2025-08-28 01:22:55 +0200

    Login Page added

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -7,20 +7,19 @@ const LoginPage: React.FC = () => {
   const handleLogin = async (data: any) => {
     setIsLoading(true);
     
-    // Placeholder login logic
+
     console.log('Login attempt with:', data);
     
-    // Simulate API call
+
     setTimeout(() => {
       console.log('Login successful');
       setIsLoading(false);
-      // TODO: Navigate to home page after successful login
-      // TODO: Store user authentication state
+
     }, 1000);
   };
 
   const handleSwitchToRegister = () => {
-    // TODO: Navigate to registration page
+
     console.log('Switch to registration page');
   };
 
