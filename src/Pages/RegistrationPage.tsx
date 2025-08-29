48bedf3755eac66c156191d82cf6fbd7224e74bb
Joseph Idowu <joeyidowu1996@gmail.com>
2025-08-30 00:37:22 +0200

    Registration page routing updated

--- src/Pages/RegistrationPage.tsx
+++ src/Pages/RegistrationPage.tsx
@@ -1,26 +1,29 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/AuthForm/AuthForm';
 
 const RegistrationPage: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleRegistration = async (data: any) => {
     setIsLoading(true);
     
-
     console.log('Registration attempt with:', data);
     
 
     setTimeout(() => {
       console.log('Registration successful');
       setIsLoading(false);
-
+      
+      // TODO: Set authentication state here
+      // For now, navigate to login
+      navigate('/login');
     }, 1000);
   };
 
   const handleSwitchToLogin = () => {
-
-    console.log('Switch to login page');
+    navigate('/login');
   };
 
   return (
